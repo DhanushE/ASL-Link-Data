b0f3bc6229c6c756864b46437d3b56c7e4fa7f76
Dhanush Eashwar <46334727+DhanushE@users.noreply.github.com>
2021-09-13 16:07:04 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+def getTitle(id=1):
+    url = f"https://www.handspeak.com/word/search/index.php?id={id}"
+    get_url = requests.get(url)
+    get_text = get_url.text
+    soup = BeautifulSoup(get_text, "html.parser")
+
+    title = list(soup.select('h1')[0].text.strip().split(" "))[-1].lower()
+    return (title, url)
+
+def generateWordsFile(starting=1, maximum=100, filename='words.json'):
+    for x in range(starting, maximum + 1):
+        title, url = getTitle(x)
+        entry = {f"{title}":f"{url}"}
+        print(title, x)
+        with open('words.json', "r+") as file:
+            data = json.load(file)
+            data.append(entry)
+            #print(data)
+            file.seek(0)
+            json.dump(data, file)
+
+if __name__ == "__main__":
+  generateWordsFile(starting=1, maximum=10464)
